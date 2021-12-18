61e6db59dc587569341c2bbcbe5a1d45fa082729
Anthony Salemo <ajssalemo@gmail.com>
2021-12-18 16:46:12 -0500

    updated the kube.sh script

--- kubernetes/kube.sh
+++ kubernetes/kube.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
 kubectl apply -f service.yaml &&
-    kubectl apply -f deployment.yaml
+    kubectl apply -f deployment.yaml && 
+    # Show services
+    echo "-------- Listing services --------"
+    kubectl get svc && 
+    # Show pods
+    echo "---------- Listing pods ----------"
+    kubectl get pods &&
+    # Show deployments
+    echo "------ Listing deployments -------"
+    kubectl get deploy &&
+    # Show replica sets
+    echo "------ Listing replica sets -------"
+    kubectl get rs
